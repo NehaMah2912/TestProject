0fb6b0f7e3e8f63a2377c82fcbb9676382f875e4
Neha <Neha@neha>
2019-02-13 05:50:59 +0530

    commit

---
+++ OCAJP/src/com/Package2/C.java
@@ -0,0 +1,23 @@
+package com.Package2;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+ 
+public class C {
+	
+	public static void main(String[] args) {
+		String[] s="My name is neha".split(" ");
+		
+		HashMap<String, Integer> hs=new HashMap<String, Integer>();
+		for()
+
+	}
+
+}
+
+
+
---
+++ OCAJP/src/com/Package2/DerivedB.java
@@ -0,0 +1,8 @@
+package com.Package2;
+
+public class DerivedB {
+	int p;
+	private int q;
+	protected int r=0;
+	public int s;
+}
